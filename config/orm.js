833e60b6571908d7618d8b93a154e494889e2e30
Julie T <joules2319@gmail.com>
2020-05-14 13:54:49 -0600

    added game selection query

---
+++ config/orm.js
@@ -0,0 +1,49 @@
+//bring in connection
+const connection = require("../config/connection.js");
+
+orm = {
+	selectAll: function (table, cb) {
+		const query = "SELECT * FROM ??";
+		connection.query(query, [table], function (err, results) {
+			if (err) throw err;
+			cb(results);
+		});
+	},
+	selectForGame: function (table, gameCol, gameName, cb) {
+		const query = "SELECT * FROM ?? WHERE ?? = ?";
+		connection.query(query, [table, gameCol, gameName], function (err, results) {
+			if (err) throw err;
+			cb(results);
+		});
+	},
+	insertOne: function (table, columns, values, cb) {
+		const query = "INSERT INTO ?? (??) VALUES (?);";
+		connection.query(query, [table, columns, values], function (err, results) {
+			if (err) throw err;
+			cb(results);
+		});
+	},
+	updateOne: function (table, column, value, conditionCol, conditionVal, cb) {
+		const query = "UPDATE ?? SET ??=? WHERE ?? = ?";
+		connection.query(
+			query,
+			[table, column, value, conditionCol, conditionVal],
+			function (err, results) {
+				if (err) throw err;
+				cb(results);
+			}
+		);
+	},
+	removeOne: function (table, conditionCol, conditionVal, cb) {
+		const query = "DELETE FROM ?? WHERE ?? = ?";
+		connection.query(query, [table, conditionCol, conditionVal], function (
+			err,
+			results
+		) {
+			if (err) throw err;
+			cb(results);
+		});
+	}
+};
+
+module.exports = orm;
